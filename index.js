d7b58202201ebf7f44735288f1461de2b43db456
Lucas Montanari <Lmonty2011@gmail.com>
2021-01-18 14:51:32 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name,email) {
+    let formData = {
+        name: name,
+        email: email
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }).then(resp => resp.json())
+    .then(json => {
+        let body = document.querySelector('body')
+        body.textContent = json.id
+    })
+    .catch(error => {
+        let body = document.querySelector('body')
+        body.textContent = error.message
+    })
+}
